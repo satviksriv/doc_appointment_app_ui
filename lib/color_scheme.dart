1c4072a68cc02d715b47ca0430160cecc3cd3fc1
satviksriv <srivastavasatvik22@gmail.com>
2021-07-10 02:58:51 +0530

    Added assets and Color Scheme

---
+++ lib/color_scheme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+final Color bgColor = Color(0xFFF9F9F9);
+final Color path1Color = Color(0xFFE4E2FF);
+final Color getStartedColorStart = Color(0xFF54D579);
+final Color getStartedColorEnd = Color(0xFF00AABF);
+final Color path2Color = Color(0xFFCEF4E8);
+final Color docBgColor = Color(0xFFE985FF);
+final Color docContentBgColor = Color(0xFFECF0F5);
+final Color dateBgColor = Color(0xFFD5E0FA);
+final Color dateColor = Color(0xFF3479CB);
